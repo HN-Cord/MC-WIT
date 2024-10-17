e29c4d8475a49aab7ebdcd3c9ccfb4a6d4e31b3b
blacknight0712 <blacknight0981@gmail.com>
2024-10-17 17:01:32 +0800

    初始提交

---
+++ src/main/java/hn/blacknight0981/wit/Wit.java
@@ -0,0 +1,17 @@
+package hn.blacknight0981.wit;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Wit extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
